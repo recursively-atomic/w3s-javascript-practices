15f40bd7c72b2d68e00bf3fcfeddce6df5a66671
Atomic <anthony.namol.090105@gmail.com>
2026-08-15 10:35:28 +0800

    feat: Add saving and loading counts to and from local storage

--- scripts/1_counter/script.js
+++ scripts/1_counter/script.js
@@ -1,29 +1,55 @@
 const countDisplay = document.getElementById('count');
 const messageDisplay = document.getElementById('message');
+const saveButton = document.getElementById('save');
+const loadButton = document.getElementById('load');
 
-function updateDisplay(value) {
-    countDisplay.innerText = value;
+countDisplay.innerText = 0;
+localStorage.clear();
+
+function updateCount(value) {
+    countDisplay.innerText = value;;
+}
+
+function updateMessage(button, string) {
+    messageDisplay.style.display = 'block';
+    messageDisplay.innerText = string;
+
+    button.disabled = true;
+
+    setTimeout(() => {
+        messageDisplay.style.display = 'none';
+        messageDisplay.innerText = '';
+
+        button.disabled = false;
+    }, 1000);
 }
 
 function increaseCount() {
     const currentValue = parseInt(countDisplay.innerText);
 
-    updateDisplay(currentValue + 1);
+    updateCount(currentValue + 1);
 }
 
 function decreaseCount() {
     const currentValue = parseInt(countDisplay.innerText);
 
-    updateDisplay(currentValue - 1);
+    updateCount(currentValue - 1);
 }
 
 function resetCount() {
-    updateDisplay(0);
+    updateCount(0);
 }
 
-function s() {
+function saveCount() {
     const currentValue = parseInt(countDisplay.innerText);
-
-    messageDisplay.style.display = 'none';
     localStorage.setItem('count', currentValue);
+
+    updateMessage(saveButton, `Count saved to ${currentValue}!`);
+}
+
+function loadCount() {
+    const previousValue = localStorage.getItem('count');
+
+    updateCount(previousValue);
+    updateMessage(loadButton, `Count loaded to ${previousValue}!`);
 }
